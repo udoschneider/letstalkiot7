ac938f2f180812cf856680890c733d2204a13862
Udo Schneider <Udo.Schneider@homeaddress.de>
2021-11-02 13:15:21 +0100

    Initial commit

---
+++ simple_c/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define VERSION "0.1"
+
+void display_help();
+
+int main(int argc, char *argv[]) {
+  int c;
+
+  while ((c = getopt(argc, argv, "v")) != -1)
+    switch (c) {
+    case 'v':
+      display_help();
+      exit(0);
+    }
+
+  printf("Hello World!\n");
+  return 0;
+}
+
+void display_help() {
+  printf("helloworld Version %s - built %s / %s\n", VERSION, __DATE__,
+         __TIME__);
+}
